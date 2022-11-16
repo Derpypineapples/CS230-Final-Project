1f4c2d28f2a27aebe314f604df13894c9137b069
Derpypineapples <112415577+Derpypineapples@users.noreply.github.com>
2022-11-16 14:11:24 -0500

    Update book.h
    Includes author, credit, and operator overload

--- book.h
+++ book.h
@@ -1,74 +1,67 @@
+/**
+ * @file book.h
+ * @author Logan Brewer
+ * @brief This file is modified from the ClientType.hpp file by Jonathan Castillo, Sean Harris, and Nadeem Ameen.
+ */
+
 #ifndef CLIENTTYPE_HPP
 #define CLIENTTYPE_HPP
 
-
-#include <string>
 #include <sstream>
+#include <string>
 using namespace std;
 
-
 class book {
-   private:
-      
-      string name;
-      string author;
-      string isbn;
-      
-   public:
-    book(string n, string a, string i);
+private:
+  string name;
+  string author;
+  string isbn;
 
-      //accessors
-      string getName() const;
-      string getAuthor() const;
-      string getISBN() const;
+public:
+  book(string n, string a, string i);
 
-      //mutators
-      void setName(string name);
-      void setAuthor(string name);
-      void setISBN(string name);
-      
-      string toString() const;
+  // accessors
+  string getName() const;
+  string getAuthor() const;
+  string getISBN() const;
+
+  // mutators
+  void setName(string name);
+  void setAuthor(string name);
+  void setISBN(string name);
+
+  string toString() const;
 };
 
-
 book::book(string n, string a, string i) {
-   this->name = n;
-   this->author = a;
-   this->isbn = i;
+  this->name = n;
+  this->author = a;
+  this->isbn = i;
 }
 
-string book::getName() const{
-   return name;
-}
+string book::getName() const { return name; }
 
-string book::getAuthor() const{
-   return author;
-}
+string book::getAuthor() const { return author; }
 
-string book::getISBN() const{
-   return isbn;
-}
+string book::getISBN() const { return isbn; }
 
-void book::setName(string n) {
-   this->name = n;
-}
+void book::setName(string n) { this->name = n; }
 
-void book::setAuthor(string a) {
-   this->author = a;
-}
+void book::setAuthor(string a) { this->author = a; }
 
-void book::setISBN(string i) {
-   this->isbn = i;
-}
+void book::setISBN(string i) { this->isbn = i; }
 
 string book::toString() const {
-   ostringstream buffer;
+  ostringstream buffer;
 
-   buffer << "Name: " << this->name << "\n"
-          << "Author: " << this->author << "\n"
-          << "ISBN: " << this->isbn << "\n";
-   return buffer.str();
+  buffer << "Name: " << this->name << "\n"
+         << "Author: " << this->author << "\n"
+         << "ISBN: " << this->isbn << "\n";
+  return buffer.str();
 }
 
+bool operator<(const book &b1, const book &b2) {
+  return ((b1.getName() == b2.getName()) ? b1.getAuthor() < b2.getAuthor() : b1.getName() < b2.getName());
+}
 
 #endif
